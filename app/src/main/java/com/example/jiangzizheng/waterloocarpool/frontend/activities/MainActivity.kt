b84ae8b2cdebe063783a9fe8653b1c2c8032a85c
Zizheng JIANG <jiangzizheng1205@gmail.com>
2018-10-28 22:40:14 -0400

    main layout

--- app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/MainActivity.kt
+++ app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.jiangzizheng.waterloocarpool.frontend.activities
 
-import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.example.jiangzizheng.waterloocarpool.R
-import com.example.jiangzizheng.waterloocarpool.backend.api.Auth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_driver -> {
+                message.setText(R.string.title_driver)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_passenger -> {
+                message.setText(R.string.title_passenger)
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onStart() {
-        super.onStart()
-        val user = Auth.instance.currentUser
-        if (user == null) {
-            startActivity(Intent(this, LoginActivity::class.java))
-            return
-        }
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
