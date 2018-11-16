af4d20f70fdbdaf09f20e32089e6e3c626dffc36
Zizheng JIANG <jiangzizheng1205@gmail.com>
2018-11-15 20:06:51 -0500

    chang to androidX

--- app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/LoginActivity.kt
+++ app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/LoginActivity.kt
@@ -2,7 +2,7 @@ package com.example.jiangzizheng.waterloocarpool.frontend.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.jiangzizheng.waterloocarpool.R
 import kotlinx.android.synthetic.main.activity_login.*
 
