1b68128cbb975dfd8647d32e13e02ac8c8a164fb
Zizheng JIANG <jiangzizheng1205@gmail.com>
2018-11-15 21:46:45 -0500

    backend

---
+++ app/src/main/java/com/example/jiangzizheng/waterloocarpool/backend/bean/Drivers.kt
@@ -0,0 +1,16 @@
+package com.example.jiangzizheng.waterloocarpool.backend.bean
+
+import java.sql.Timestamp
+
+data class Drivers(
+    var dCity: String = "",
+    var dAddress: String = "",
+
+    var aCity: String = "",
+    var aAddress: String = "",
+
+    var ddate: Timestamp = Timestamp(0),
+    var phoneNumber: String = "",
+    var vacancies: Int = 0,
+    var price: Double = 0.0
+)
