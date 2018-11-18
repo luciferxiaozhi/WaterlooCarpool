531d41f692fdbdca3377c96a205224b125c0ec5d
luciferxiaozhi <luciferxiaozhi@163.com>
2018-11-18 18:54:20 -0500

    Add account Information functionality

--- app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/AccountInfoActivity.kt
+++ app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/AccountInfoActivity.kt
@@ -2,13 +2,28 @@ package com.example.jiangzizheng.waterloocarpool.frontend.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.widget.Toolbar
 import com.example.jiangzizheng.waterloocarpool.R
+import com.example.jiangzizheng.waterloocarpool.backend.api.Store
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AccountInfoActivity : AppCompatActivity() {
 
     var toolbar: Toolbar? = null
+    var currUser = FirebaseAuth.getInstance().currentUser
+    var db = FirebaseFirestore.getInstance()
+    val uid = currUser?.uid.toString()
+    val docRef = db.collection("users").document(uid)
+    val userEmail = currUser?.email
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +33,30 @@ class AccountInfoActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        readDataAndSetText()
+
+
     }
+
+    fun readDataAndSetText()
+    {
+        docRef.get().addOnCompleteListener(this) {task ->
+            if (task.isSuccessful)
+            {
+                val doc = task.getResult()
+                val textFName = findViewById<TextView>(R.id.account_info_first_name)
+                val textLName = findViewById<TextView>(R.id.account_info_last_name)
+                val textEmail = findViewById<TextView>(R.id.account_info_email)
+
+                textFName.setText(doc?.get("firstName").toString())
+                textLName.setText(doc?.get("lastName").toString())
+                textEmail.setText(userEmail)
+            }
+        }
+
+    }
+
+
 }
 
