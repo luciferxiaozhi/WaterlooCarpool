3378748ff731af79dec7f970bc915a4469989781
Zizheng JIANG <jiangzizheng1205@gmail.com>
2018-11-21 19:39:05 -0500

    syntax fixed

--- app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/AccountInfoActivity.kt
+++ app/src/main/java/com/example/jiangzizheng/waterloocarpool/frontend/activities/AccountInfoActivity.kt
@@ -6,47 +6,41 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.widget.Toolbar
 import com.example.jiangzizheng.waterloocarpool.R
 import com.example.jiangzizheng.waterloocarpool.backend.api.Auth
-import com.example.jiangzizheng.waterloocarpool.backend.api.Store
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
-import com.google.firebase.storage.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.io.IOException
-import java.lang.Exception
 import java.util.*
 import kotlin.collections.HashMap
 
 class AccountInfoActivity : AppCompatActivity() {
 
     // information related variables
-    var currUser = FirebaseAuth.getInstance().currentUser
+    private var currUser = FirebaseAuth.getInstance().currentUser
     var db = FirebaseFirestore.getInstance()
-    val uid = currUser?.uid.toString()
-    val docRef = db.collection("users").document(uid)
-    val userEmail = currUser?.email
+
+    private val uid = currUser?.uid.toString()
+    private val docRef = db.collection("users").document(uid)
+    private val userEmail = currUser?.email
 
     // Icon related variables
     var btnUpload: Button? = null
     var btnChoose: Button? = null
     var imageView: ImageView? = null
     var filePath: Uri? = null
-    val PICK_IMAGE_REQUEST: Int = 71
-    val storage: FirebaseStorage = FirebaseStorage.getInstance()
-    val storageReference: StorageReference = storage.getReference()
-    var imageUID: String? = null
+
+    private val pickImgReq: Int = 71
+    private val storage: FirebaseStorage = FirebaseStorage.getInstance()
+    private val storageReference: StorageReference = storage.reference
+    private var imageUID: String? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,31 +64,29 @@ class AccountInfoActivity : AppCompatActivity() {
         readDataAndSetText()
     }
 
-    fun readDataAndSetText() {
+    private fun readDataAndSetText() {
         docRef.get().addOnCompleteListener(this) { task ->
             if (task.isSuccessful) {
-                val doc = task.getResult()
+                val doc = task.result
                 val textFName = findViewById<TextView>(R.id.account_info_first_name)
                 val textLName = findViewById<TextView>(R.id.account_info_last_name)
                 val textEmail = findViewById<TextView>(R.id.account_info_email)
 
-                textFName.setText(doc?.get("firstName").toString())
-                textLName.setText(doc?.get("lastName").toString())
-                textEmail.setText(userEmail)
+                textFName.text = doc?.get("firstName").toString()
+                textLName.text = doc?.get("lastName").toString()
+                textEmail.text = userEmail
             }
         }
     }
 
     private fun chooseImage() {
-        val intent: Intent = Intent()
-        intent.setType("image/*")
-        intent.setAction(Intent.ACTION_GET_CONTENT)
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+        Intent().type = "image/*"
+        Intent().action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(Intent(), "Select Picture"), pickImgReq)
     }
 
     private fun uploadImage() {
-        if (filePath != null)
-        {44
+        if (filePath != null) {
             val progressDialog: ProgressDialog = ProgressDialog(this)
             progressDialog.setTitle("Uploading...")
             progressDialog.show()
@@ -102,38 +94,32 @@ class AccountInfoActivity : AppCompatActivity() {
 
             val ref: StorageReference = storageReference.child("image/$imageUID")
             ref.putFile(filePath as Uri)
-                .addOnSuccessListener(object: OnSuccessListener<UploadTask.TaskSnapshot>{
-                    override fun onSuccess(p0: UploadTask.TaskSnapshot?) {
-                        progressDialog.dismiss()
-                        Toast.makeText(baseContext, "Uploaded", Toast.LENGTH_SHORT).show()
-                        saveUIDToFireStore(imageUID)
-                    }
-                })
-                .addOnFailureListener(object: OnFailureListener{
-                    override fun onFailure(e: Exception) {
-                        progressDialog.dismiss()
-                        Toast.makeText(baseContext, "Failed "+ e.message, Toast.LENGTH_SHORT).show()
-                    }
-                })
+                .addOnSuccessListener {
+                    progressDialog.dismiss()
+                    Toast.makeText(baseContext, "Uploaded", Toast.LENGTH_SHORT).show()
+                    saveUIDToFireStore(imageUID)
+                }
+                .addOnFailureListener { e ->
+                    progressDialog.dismiss()
+                    Toast.makeText(baseContext, "Failed " + e.message, Toast.LENGTH_SHORT).show()
+                }
 
-                .addOnProgressListener(object: OnProgressListener<UploadTask.TaskSnapshot>{
-                    override fun onProgress(taskSnapshot: UploadTask.TaskSnapshot?) {
-                        val progress: Double = (100.0* taskSnapshot!!.bytesTransferred /taskSnapshot
-                            .totalByteCount)
-                        progressDialog.setMessage("Uploaded ${progress.toInt()}%")
-                    }
-                })
+                .addOnProgressListener { taskSnapshot ->
+                    val progress: Double = (100.0 * taskSnapshot!!.bytesTransferred / taskSnapshot
+                        .totalByteCount)
+                    progressDialog.setMessage("Uploaded ${progress.toInt()}%")
+                }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-            && data != null && data.getData() != null
+        if (requestCode == pickImgReq && resultCode == RESULT_OK
+            && data != null && data.data != null
         ) {
-            filePath = data.getData()
+            filePath = data.data
             try {
-                val bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath)
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
                 imageView?.setImageBitmap(bitmap)
             } catch (e: IOException) {
                 e.printStackTrace()
@@ -141,14 +127,13 @@ class AccountInfoActivity : AppCompatActivity() {
         }
     }
 
-    fun saveUIDToFireStore(UID: String?)
-    {
+    private fun saveUIDToFireStore(UID: String?) {
         val user = Auth.instance.currentUser
         val db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("users").document(user!!.uid.toString())
+        val docRef = db.collection("users").document(user!!.uid)
 
-        val UIDMap: HashMap<String, String?> = hashMapOf("iconUID" to UID)
-        docRef.set(UIDMap as Map<String, String?>, SetOptions.merge())
+        val uidMap: HashMap<String, String?> = hashMapOf("iconUID" to UID)
+        docRef.set(uidMap as Map<String, String?>, SetOptions.merge())
     }
 
 
